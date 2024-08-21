736efc90c577306757c817f543c6f3d914ffb704
pietra202 <pipispietragodoifelipe@gmail.com>
2024-08-21 11:12:59 -0300

    ajustes

--- EstruturaDeDados/EstruturaDeDados/Program.cs
+++ EstruturaDeDados/EstruturaDeDados/Program.cs
@@ -3,7 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Minha primeira branch");
-        Console.WriteLine("Trabalhando com Git\Github");
+        Console.WriteLine("Trabalhando com Git\\Github");
         Console.WriteLine("Mais uma tentativa de trabalha com Git");
     }
 }
